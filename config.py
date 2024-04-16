39dd2fbe91cdadf3bd69346bc41a6b69608c79f8
Web-projektit <jukka.aula@omnia.fi>
2024-04-16 11:31:11 +0300

    Azure

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
@@ -25,15 +24,15 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    DB_USERNAME = os.environ.get('LOCAL_DB_USERNAME') or 'root'
-    DB_PASSWORD = os.environ.get('LOCAL_DB_PASSWORD') or ''
-    DB_NAME = os.environ.get('LOCAL_DB_NAME') or 'flask_sovellusmalli'
-    DB_SERVER = os.environ.get('LOCAL_DB_SERVER') or 'localhost'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + DB_USERNAME + ':' + DB_PASSWORD + '@' + DB_SERVER + ':3306/' + DB_NAME
+    DB_USERNAME = os.environ.get('DB_USERNAME') or 'root'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') or ''
+    DB_NAME = os.environ.get('DB_NAME') or 'flask_sovellusmalli'
+    DB_SERVER = os.environ.get('DB_SERVER') or 'localhost'
+    DB_PORT = os.environ.get('DB_PORT') or '3306'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + DB_USERNAME + ':' + DB_PASSWORD + '@' + DB_SERVER + ':' + DB_PORT +'/' + DB_NAME
     WTF_CSRF_ENABLED = True
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
     #    'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-
 
 class TestingConfig(Config):
     TESTING = True
