767bdd079eb4bb8d5e1e0854054905f0f0f038b0
Web-projektit <jukka.aula@omnia.fi>
2024-05-02 12:54:03 +0300

    restapi

--- app/__init__.py
+++ app/__init__.py
@@ -37,4 +37,8 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    from .restapi import restapi as restapi_blueprint
+    app.register_blueprint(restapi_blueprint, url_prefix='/restapi')
+
+
     return app
